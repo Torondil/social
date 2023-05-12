fdb929a95d5c56225bb4eb1b62e344bbc6f1f56b
torondil <bulavskipavel@gmail.com>
2023-05-12 12:10:15 +0200

    feat: add basic structure of project, add PostList component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './styles/App.css';
+import PostList from './components/PostList';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id:1, title: 'Javascript', body: 'Description'},
+    {id:2, title: 'Javascript2', body: 'Description2'},
+    {id:3, title: 'Javascript3', body: 'Description3'},
+  ])
+
+  const [posts2, setPosts2] = useState([
+    {id:1, title: 'Python', body: 'Description'},
+    {id:2, title: 'Python2', body: 'Description2'},
+    {id:3, title: 'Python3', body: 'Description3'},
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PostList posts={posts} title="Посты про JS"/>
+      <PostList posts={posts2} title="Посты про Python"/>
     </div>
   );
 }
