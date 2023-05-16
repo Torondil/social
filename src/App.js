6f4c1ea0250e2868770c8aa4359c3ffbc010e4f7
torondil <bulavskipavel@gmail.com>
2023-05-16 17:02:48 +0200

    feat: add API data and animation loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
@@ -6,16 +6,32 @@ import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/MyModal/MyModal';
 import MyButton from './components/UI/button/MyButton';
 import { usePosts } from './hooks/usePost';
+import PostService from './API/PostService';
+import Loader from './components/UI/Loader/Loader';
 
 function App() {
   const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostLoading, setIsPostLoading] = useState(false);
+  
+  useEffect(() => {
+    fetchPosts()
+  }, [])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
     setModal(false)
+  }
+
+  async function fetchPosts() {
+    setIsPostLoading(true);
+    setTimeout(async () => {
+      const posts = await PostService.getAll();
+      setPosts(posts);
+      setIsPostLoading(false);
+    }, 1000)
   }
  
   // Получаем пост из дочернего компонента
@@ -25,6 +41,7 @@ function App() {
 
   return (
     <div className='App'>
+      <button onClick={fetchPosts}>GET POSTS</button>
       <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
         Создать пользователя
       </MyButton>
@@ -33,7 +50,12 @@ function App() {
       </MyModal>
       <hr style={{margin: '15px 0'}}></hr>
       <PostFilter filter={filter} setFilter={setFilter}/>
-      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
+      {isPostLoading
+        ? <div style={{display:'flex', justifyContent:'center', marginTop:'50px'}}>
+          <Loader/>
+        </div>
+        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
+      }
     </div>
   );
 }
