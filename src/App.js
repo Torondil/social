9e7d703719be584c6370910d0b14bd4e3fc9770d
torondil <bulavskipavel@gmail.com>
2023-05-24 19:08:38 +0200

    feat: add browserrouting

--- src/App.js
+++ src/App.js
@@ -1,85 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './styles/App.css';
-import PostList from './components/PostList';
-import PostForm from './components/PostForm';
-import PostFilter from './components/PostFilter';
-import MyModal from './components/UI/MyModal/MyModal';
-import MyButton from './components/UI/button/MyButton';
-import { usePosts } from './hooks/usePost';
-import PostService from './API/PostService';
-import Loader from './components/UI/Loader/Loader';
-import { useFetching } from './hooks/useFetching';
-import { getPageCount, getPagesArray } from './utils/pages';
-import Pagination from './components/UI/pagination/Pagination';
+import {BrowserRouter} from 'react-router-dom'
+import Navbar from './components/UI/Navbar/Navbar';
+import AppRouter from './components/AppRouter';
 
 function App() {
-  const [posts, setPosts] = useState([])
-  const [filter, setFilter] = useState({sort: '', query: ''});
-  const [modal, setModal] = useState(false);
-
-  // состояние для пагинации
-  const [totalPages, setTotalPages] = useState(0)
-  const [limit, setlimit] = useState(10)
-  const [page, setPage] = useState(1)
-
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  
-  // хук который обрабатывает индикации загрузки и ошибки запроса на получение данных
-  const [fetchPosts, isPostLoading, postError] = useFetching(async (limit, page) => {
-    const response = await PostService.getAll(limit, page);
-    setPosts(response.data);
-    const totalCount = response.headers['x-total-count']
-    setTotalPages(getPageCount(totalCount, limit))
-  })
-  
-  // вызываем один раз массив постов при загрузке страницы (массив пустой поэтому только один раз)
-  useEffect(() => {
-    fetchPosts(limit, page)
-  }, [])
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost])
-    setModal(false)
-  }
- 
-  // Получаем пост из дочернего компонента
-  const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id))
-  }
-
-  const changePage = (page) => {
-    setPage(page)
-    fetchPosts(limit, page)
-  }
-
-
   return (
-    <div className='App'>
-      <button onClick={fetchPosts}>GET POSTS</button>
-      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
-        Создать пользователя
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
-      <hr style={{margin: '15px 0'}}></hr>
-      <PostFilter filter={filter} setFilter={setFilter}/>
-      {postError &&
-        <h1>Произошла ошибка ${postError}</h1>
-      }
-      {isPostLoading
-        ? <div style={{display:'flex', justifyContent:'center', marginTop:'50px'}}>
-          <Loader/>
-        </div>
-        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
-      }
-      <Pagination
-        page={page}
-        changePage={changePage}
-        totalPages={totalPages}
-      />
-    </div>
-  );
+    <BrowserRouter>
+      <Navbar/>
+      <AppRouter/>
+    </BrowserRouter>
+  )
 }
 
 export default App;
