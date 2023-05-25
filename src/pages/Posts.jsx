1eb8cc26c188dcda64bdd133bf515b5e04d4536c
torondil <bulavskipavel@gmail.com>
2023-05-25 19:42:12 +0200

    feat: add endless tape instead of pagination

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PostList from '../components/PostList';
 import PostForm from '../components/PostForm';
 import PostFilter from '../components/PostFilter';
@@ -10,15 +10,20 @@ import Loader from '../components/UI/Loader/Loader';
 import { useFetching } from '../hooks/useFetching';
 import { getPageCount } from '../utils/pages';
 import Pagination from '../components/UI/pagination/Pagination';
+import { useObserver } from '../hooks/useObserver';
+import MySelect from '../components/UI/select/MySelect';
 
 function Posts() {
   const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
 
+  // обсервер для бесконечной ленты
+  const lastElement = useRef()
+
   // состояние для пагинации
   const [totalPages, setTotalPages] = useState(0)
-  const [limit] = useState(10)
+  const [limit, setLimit] = useState(5)
   const [page, setPage] = useState(1)
 
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
@@ -26,15 +31,19 @@ function Posts() {
   // хук который обрабатывает индикации загрузки и ошибки запроса на получение данных
   const [fetchPosts, isPostLoading, postError] = useFetching(async (limit, page) => {
     const response = await PostService.getAll(limit, page);
-    setPosts(response.data);
+    setPosts([...posts, ...response.data]);
     const totalCount = response.headers['x-total-count']
     setTotalPages(getPageCount(totalCount, limit))
   })
-  
+
+  useObserver(lastElement, page < totalPages, isPostLoading, () => {
+    setPage(page + 1)
+  })
+
   // вызываем один раз массив постов при загрузке страницы (массив пустой поэтому только один раз)
   useEffect(() => {
     fetchPosts(limit, page)
-  }, [])
+  }, [page, limit])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -48,7 +57,6 @@ function Posts() {
 
   const changePage = (page) => {
     setPage(page)
-    fetchPosts(limit, page)
   }
 
 
@@ -63,14 +71,26 @@ function Posts() {
       </MyModal>
       <hr style={{margin: '15px 0'}}></hr>
       <PostFilter filter={filter} setFilter={setFilter}/>
+      <MySelect
+        value={limit}
+        onChange={value => setLimit(value)}
+        defaultValue={"Кол-во элементов на странице"}
+        options={[
+          {value: 5, name: '5'},
+          {value: 10, name: '10'},
+          {value: 25, name: '25'},
+          {value: -1, name: 'Показать все'},
+        ]}
+      />
       {postError &&
         <h1>Произошла ошибка ${postError}</h1>
       }
-      {isPostLoading
-        ? <div style={{display:'flex', justifyContent:'center', marginTop:'50px'}}>
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
+      <div ref={lastElement} style={{height: 20, background: 'red'}}></div>
+      {isPostLoading &&
+        <div style={{display:'flex', justifyContent:'center', marginTop:'50px'}}>
           <Loader/>
         </div>
-        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
       }
       <Pagination
         page={page}
