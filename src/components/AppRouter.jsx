a3697afc86ee654f431fcc3f43a7e8424ebd62f9
torondil <bulavskipavel@gmail.com>
2023-05-25 17:38:10 +0200

    fix: decompose AppRouter component with route.js

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,18 +1,43 @@
+// import React from 'react'
+// import { Navigate, Route, Routes } from 'react-router-dom'
+// import About from '../pages/About'
+// import Posts from '../pages/Posts'
+// import Error from '../pages/Error'
+// import PostIdPage from '../pages/PostIdPage'
+
+// const AppRouter = () => {
+//   return (
+//     <Routes>
+//         <Route path='/about' element={<About />} />
+//         <Route path='/posts' element={<Posts />} />
+//         <Route path='/posts/:id' element={<PostIdPage />} />
+//         <Route path="/error" element={<Error />} />
+//         <Route path="/*" element={<Navigate to="/error" replace />} />
+//     </Routes>
+//   )
+// }
+
+// export default AppRouter
+
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
-import About from '../pages/About'
-import Posts from '../pages/Posts'
-import Error from '../pages/Error'
-import PostIdPage from '../pages/PostIdPage'
+import { routes } from '../router/routes'
 
 const AppRouter = () => {
   return (
     <Routes>
-        <Route path='/about' element={<About />} />
-        <Route path='/posts' element={<Posts />} />
-        <Route path='/posts/:id' element={<PostIdPage />} />
-        <Route path="/error" element={<Error />} />
-        <Route path="/*" element={<Navigate to="/error" replace />} />
+        {routes.map((route, index) => {
+          return (
+          <Route
+            key = {index}
+            path = {route.path}
+            element = {<route.element/>}
+           />
+          )
+        })
+        }
+        <Route path="/*" element={<Navigate to="/error" replace/>} />
+        
     </Routes>
   )
 }
